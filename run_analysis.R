ff2a271b3e1b02b985b7dc6983832324f11a9ea1
jimjjr <jimjelincic@gmail.com>
2015-03-20 03:06:51 -0500

    Modified script to output file and removed obsolete text.

--- run_analysis.R
+++ run_analysis.R
@@ -9,23 +9,10 @@ if(length(missing_packages)) {
 ## Load required packages
 library(dplyr)
 
-### Need to add code to download and extract data ###
-#zip_data_file <- list.files(pattern="*.zip")[1]
-#read_zip <- (!data_dir %in% list.files())
-
-## Extracts data from zip file if not already extracted
-# if(read_zip){
-#     unzip(zip_data_file)
-# }
-
+# Define file paths
 data_dir <- "UCI HAR Dataset"
-
 activity_labels_file <- file.path(data_dir, "activity_labels.txt")
 data_features_file <- file.path(data_dir, "features.txt")
-keep_features <- c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 
-                   227:228, 240:241, 253:254, 266:271, 294:296, 345:350, 
-                   373:375, 424:429, 452:454, 503:504, 513, 516:517, 526, 
-                   529:530, 539, 542:543, 552)
 
 test_data_file <- file.path(data_dir, "test/X_test.txt")
 test_labels_file <- file.path(data_dir, "test/y_test.txt")
@@ -34,6 +21,12 @@ test_subject_file <- file.path(data_dir, "test/subject_test.txt")
 train_data_file <- file.path(data_dir, "train/X_train.txt")
 train_labels_file <- file.path(data_dir, "/train/y_train.txt")
 train_subject_file <- file.path(data_dir, "/train/subject_train.txt")
+
+# Previously-determined data columns with "average" or "Standard deviation"
+keep_features <- c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 
+                   227:228, 240:241, 253:254, 266:271, 294:296, 345:350, 
+                   373:375, 424:429, 452:454, 503:504, 513, 516:517, 526, 
+                   529:530, 539, 542:543, 552)
 
 ## Read in activity definitions
 activity_definitions <- read.table(activity_labels_file,
@@ -68,13 +61,7 @@ all_data <- cbind(all_data,
                   rbind(test_labels, train_labels), 
                   rbind(test_subject, train_subject))
 
-#### Obsoleted ####
-## Remove everything but averages, standard deviations, activity labels, and
-## subject numbers from data
-#### all_data <- all_data[,grepl("mean|std|activity_label|subject_number",
-####                            colnames(all_data), ignore.case=TRUE)]
-
-## Define column names
+## Define column names to something meaningful
 col_names <- c("Mean of time-domain body linear acceleration for x-axis",
                "Mean of time-domain body linear acceleration for y-axis",
                "Mean of time-domain body linear acceleration for z-axis",
@@ -168,31 +155,9 @@ all_data[,"Activity_Label"] <- factor(all_data[,"Activity_Label"],
 ## Convert subject numbers to factor
 all_data <- mutate(all_data, Subject_Number = as.factor(Subject_Number))
 
-
-### Obsoleted
-# ## Make column labels meaningful
-# colnames(all_data) <- sub("^t", "Time-Domain_", colnames(all_data))
-# colnames(all_data) <- sub("^f", "FFT_of_", colnames(all_data))
-# colnames(all_data) <- sub("(.*).mean(.*)", "Mean_of_\\1\\2", colnames(all_data))
-# colnames(all_data) <- sub("(.*)std(.*)", "StandardDeviation_of_\\1\\2",
-#                           colnames(all_data))
-# colnames(all_data) <- sub("Acc", "AccelerometerSignal", colnames(all_data))
-# colnames(all_data) <- sub("Gyro", "GyroscopeSignal", colnames(all_data))
-# colnames(all_data) <- sub("Mag", "Magnitude", colnames(all_data))
-# colnames(all_data) <- sub("Freq", "Frequency", colnames(all_data))
-# colnames(all_data) <- sub("(*.)X(.+)", "\\1X_axis\\2", colnames(all_data))
-# colnames(all_data) <- sub("(*.)Y(.+)", "\\1Y_axis\\2", colnames(all_data))
-# colnames(all_data) <- sub("(*.)Z(.+)", "\\1Z_axis\\2", colnames(all_data))
-# colnames(all_data) <- sub("(.*)X$", "\\1\\2_in_X_axis", colnames(all_data))
-# colnames(all_data) <- sub("(.*)Y$", "\\1\\2_in_Y_axis", colnames(all_data))
-# colnames(all_data) <- sub("(.*)Z$", "\\1\\2_in_Z_axis", colnames(all_data))
-# colnames(all_data) <- sub("angle.t", "Angle_between_Time-Domain_",
-#                           colnames(all_data))
-# colnames(all_data) <- sub("^angle", "Angle_between_", colnames(all_data))
-# colnames(all_data) <- sub("^Angle(.*)gravity(.*)", "Angle\\1_and_Gravity\\2",
-#                           colnames(all_data))
-# colnames(all_data) <- gsub("\\.", "", colnames(all_data))
-
+## Take mean of every variable by activity and subject
 summarized_data <- all_data %>% 
     group_by(Activity_Label, Subject_Number) %>%
     summarise_each(funs(mean))
+
+write.table(summarized_data, "tidy_summary.txt", row.name=FALSE)
